39e3ffe621b9fe29a2ed9658f6bcbe97dd8eb059
Eric Laursen <ericjlaursen@gmail.com>
2020-01-25 17:52:16 -0500

    App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,69 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import {AppContext} from './State/Context'
-
+import './App.css'
 const App = () => {
-		const ctxVals = useContext(AppContext);
-		console.log('ctxVals')
-		console.log(ctxVals)
-
+	
+	const {formState, updateFormData} = useContext(AppContext);
+	
+	let questionSections = formState && Object.keys(formState)
+	
 	return(		
 		<main className="container">
 				<h3>Overhead Assessment</h3>
 				<form>
-					<p> test p </p>
+					{ formState && 
+						questionSections &&
+						questionSections.map((section, idx) => {
+							
+							let thisSectionQuestions = formState[section]
+
+							return(
+								<Fragment key={`${section}-${idx}`}>
+									<h3>{section}</h3>
+									<section className="conditional-block">
+									{/* UN-Conditional Questions*/}
+									{thisSectionQuestions.map((q, qidx) => {
+
+										let conditionalQuestionIsTrue = false;
+										let isConditional = q.conditional
+										
+										let conditionalQStatus = false;
+										if(isConditional){
+											conditionalQStatus = thisSectionQuestions.filter(d => d.question == q.on)[0].answer
+										} 
+										if(
+											q.conditional == false || 
+											conditionalQStatus == true
+										){
+											return(
+												<label 
+														htmlFor={q.question} 
+														key={`question-${qidx}`}
+														className={`question-label ${conditionalQStatus == true && 'conditional'}`}
+													>
+														{q.question}
+														<input 
+															id={`question-${qidx}`} 
+															checked={q.answer}
+															onChange={() => {
+																let thisObj = {
+																	index: qidx,
+																	section,
+																	question: q.question,
+																	answer: !q.answer
+																}
+																updateFormData(thisObj, formState)
+															}}
+															type={q.type} />
+												</label>
+											)	
+										}
+									}).filter(d => d)}
+								</section>
+								</Fragment>
+							)
+						})
+					}
 				</form>
 		</main>
 	)
