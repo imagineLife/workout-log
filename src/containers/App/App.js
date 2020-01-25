20bac400f3ec5a1013accfafbd7d3164aff4b2bc
Eric Laursen <ericjlaursen@gmail.com>
2020-01-25 16:07:25 -0500

    init

---
+++ src/containers/App/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => (<p>App</p>);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import './index.css'
+import Container from './containers/App'
+
+ReactDOM.render(<Container />, document.getElementById("app"));
