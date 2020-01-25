cba303e9c69f3be46a5d105a67e6f14829445c4f
Eric Laursen <ericjlaursen@gmail.com>
2020-01-25 16:21:03 -0500

    imports context

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {AppContext} from './State/Context'
 
-const App = () => (
-	<main className="container">
-			<p>Test</p>
-	</main>
-	);
+const App = () => {
+		const ctxVals = useContext(AppContext);
+		console.log('ctxVals')
+		console.log(ctxVals)
+
+	return(		
+		<main className="container">
+				<h3>Overhead Assessment</h3>
+				<form>
+					<p> test p </p>
+				</form>
+		</main>
+	)
+};
 
 export default App;
