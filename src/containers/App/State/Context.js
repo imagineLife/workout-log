8b8905573b34d76618159cefe4a5108132d9075c
Eric Laursen <ericjlaursen@gmail.com>
2020-01-25 17:52:03 -0500

    updateFormData works

--- src/containers/App/State/Context.js
+++ src/containers/App/State/Context.js
@@ -1,12 +1,36 @@
-import React, { createContext } from 'react'
-
+import React, { createContext, useState } from 'react'
+import feetQs from './questions/feet.json'
 const AppContext = createContext()
 
 const {Provider, Consumer} = AppContext;
 
 const AppProvider = ({children}) => {
+
+	let [formState, setFormState] = useState({
+		'Feet': feetQs
+	})
+
+	let updateFormData = (thisObj, curState) => {		
+		let stateClone = {...curState}
+		let thisSectionFromState = stateClone[thisObj.section]
+		let thisQuestionFromState = thisSectionFromState.filter((d,idx) => idx == thisObj.index)[0]
+		let newQuestionObj = {...thisQuestionFromState, ...thisObj}
+		
+		delete newQuestionObj.section;
+		delete newQuestionObj.index;
+		
+		let newSection = thisSectionFromState.map((s, idx) => {
+			if(idx === thisObj.index) return newQuestionObj
+				return s
+		})
+		
+		let newState = {...stateClone, ...{[thisObj.section]: newSection}}
+		
+		setFormState(newState)
+	}
+
 	return(
-		<Provider value={{demo: 'value'}}>
+		<Provider value={{formState, updateFormData}}>
 			{children}
 		</Provider>
 	)
